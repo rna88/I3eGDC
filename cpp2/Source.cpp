d73e29fda60b5e5bbb13ce9bae9f6aa06ed37226
rna88 <cinnuv@gmail.com>
2015-03-08 15:19:20 -0700

    Changed 32 bit floats to 64bit, and added new font.

--- cpp2/Source.cpp
+++ cpp2/Source.cpp
@@ -15,7 +15,7 @@ using namespace irrklang;
 //core::vector2d<f32>(ballx, bally);
 core::vector2d<s32> ballPosition(0.0f, 0.0f);
 core::vector2d<s32> ballDirection(1.0f,1.0f);
-
+f64 ballspeed = 0.2f;
 
 int main()
 {
@@ -52,10 +52,10 @@ int main()
 
 
 	// The time when the frame starts
-	f32 now = device->getTimer()->getTime();
-	f32 then = 0;
-	f32 deltaTime = 0;
-	f32 fpsFromDeltaTime = 0;
+	f64 now = device->getTimer()->getTime();
+	f64 then = 0;
+	f64 deltaTime = 0;
+	f64 fpsFromDeltaTime = 0;
 
 	while (device->run())
 	{
@@ -72,10 +72,10 @@ int main()
 		driver->beginScene(1,1,video::SColor(255,100,100,100));
 
 		//x = x + (speed*dt)*direction.X
-		ballPosition.X += (0.2f * deltaTime)*ballDirection.X;
+		ballPosition.X += (ballspeed * deltaTime)*ballDirection.X;
 		if (ballPosition.X > 700 || ballPosition.X < 0) ballDirection.X *= -1;
 
-		ballPosition.Y += (0.2f * deltaTime)*ballDirection.Y;
+		ballPosition.Y += (ballspeed * deltaTime)*ballDirection.Y;
 		if (ballPosition.Y > 500 || ballPosition.Y < 0) ballDirection.Y *= -1;
 
 		driver->draw2DImage(ball, ballPosition);
